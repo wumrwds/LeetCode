0a203b829046deac5aa416a98d9c659d2382c82d
Minreng Wu <wumrwds@gmail.com>
2019-11-28 14:47:55 -0600

    feat: uploaded LeetCode 239

--- SlidingWindow/239_Sliding_Window_Maximum.java
+++ SlidingWindow/239_Sliding_Window_Maximum.java
@@ -33,3 +33,54 @@ class Solution {
         return max;
     }
 }
+
+
+class Solution {
+    /*
+    
+    nums[]      = 1 3 -1 | -3 5 3 | 6 7
+    maxToLeft   = 1 3  3   -3 5 5   6 7
+    maxToRight  = 3 3 -1    5 5 3   7 7 
+    
+    ans  = Math.max(toLeft[i+k-1], toRight[i])
+    */
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int n = nums.length;
+        if (n == 0) {
+            return new int[0];
+        }
+        
+        int[] maxToLeft = new int[n], maxToRight = new int[n];
+        
+        int max = nums[0];
+        for (int i = 0; i < n; i++) {
+            if (i % k == 0) {
+                max = nums[i];
+            }
+            else {
+                max = Math.max(max, nums[i]);
+            }
+            maxToLeft[i] = max;
+        }
+        System.out.println(Arrays.toString(maxToLeft));
+        
+        max = nums[n-1];
+        for (int i = n - 1; i >= 0; i--) {
+            if ( (i+1) % k == 0) {
+                max = nums[i];
+            }
+            else {
+                max = Math.max(max, nums[i]);
+            }
+            maxToRight[i] = max;
+        }
+        System.out.println(Arrays.toString(maxToRight));
+        
+        int[] ans = new int[n-k+1];
+        for (int i = 0; i + k - 1 < n; i++) {
+            ans[i] = Math.max(maxToLeft[i+k-1], maxToRight[i]);
+        }
+        
+        return ans;
+    }
+}
