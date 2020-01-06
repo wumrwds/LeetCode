8ea5d755e5d5d8595f2463756de28d38d395a411
Minreng Wu <wumrwds@gmail.com>
2020-01-06 22:16:33 +0800

    feat: uploaded LeetCode 204

---
+++ Math/204_Count_Primes.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int countPrimes(int n) {
+        boolean[] notPrime = new boolean[n];
+        for (int i = 2; i * i < n; i++) {
+            if (!notPrime[i]) {
+                for (int j = i * i; j < n; j += i) {
+                    notPrime[j] = true;
+                }
+            }
+        }
+
+        int cnt = 0;
+        for (int i = 2; i < n; i++) {
+            if (!notPrime[i]) {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+}
